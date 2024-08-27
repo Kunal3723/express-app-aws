56679df023d6726150256469105db87fc6c94302
kunal3723 <kunal.kg8@gmail.com>
2024-08-27 12:10:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// Load express module
+const express = require('express');
+const app = express();
+
+// Set the port
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
